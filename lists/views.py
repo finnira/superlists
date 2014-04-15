a8c5ca2e208fca0ea8da56abce72a44e94e02bbd
Finnira <fin.harr@dfgh.net>
2014-04-15 19:34:26 +0100

    Can save one item

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text','')
+        return render(request, 'home.html',
+                      {'new_item_text': new_item_text})
+    else:
+        return render(request, 'home.html',
+                      {'new_item_text':''})
+
