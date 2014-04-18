62033c63151e8ae5e4ce26b6c605d76acffd6f6f
Finnira <fin.harr@dfgh.net>
2014-04-18 19:17:22 +0100

    items saved to DB and displayed in list

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Item
+
 
 def home_page(request):
 
     if request.method == 'POST':
-        new_item_text = request.POST.get('item_text','')
-        return render(request, 'home.html',
-                      {'new_item_text': new_item_text})
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
     else:
+        items = Item.objects.all()
         return render(request, 'home.html',
-                      {'new_item_text':''})
+                      {'items':items})
 
+    #todo: more than one list
