1ee36a8ffe0f3c82e91a04af485f7c5d6d1db812
Finnira <fin.harr@dfgh.net>
2014-04-18 19:17:22 +0100

    418

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+
 
 def home_page(request):
 
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text','')
-        return render(request, 'home.html',
-                      {'new_item_text': new_item_text})
+        Item.objects.create(text=new_item_text)
     else:
-        return render(request, 'home.html',
-                      {'new_item_text':''})
+        new_item_text = ''
 
+    return render(request, 'home.html',
+                  {'new_item_text':new_item_text})
+
+    #todo: display multiple items in the table
+    #todo: more than one list
